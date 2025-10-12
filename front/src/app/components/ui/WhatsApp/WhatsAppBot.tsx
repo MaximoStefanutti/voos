2a4595d81a1fee59c1611c155869c4530f2878ce
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2025-10-12 19:26:47 -0300

    modificación en el css de whatsappbot logo

--- front/src/app/components/ui/WhatsApp/WhatsAppBot.tsx
+++ front/src/app/components/ui/WhatsApp/WhatsAppBot.tsx
@@ -2,8 +2,6 @@
 
 import { WhatsApp } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
-// import { anime } from "animejs";
-import { useEffect, useRef } from "react";
 
 interface WhatsAppBotProps {
   phone: string; // número con código de pías sin + ni espacios, ej: "5493513513351"
@@ -14,23 +12,9 @@ export function WhatsAppBot({
   phone,
   message = "¡Hola! Me gustaría obtener más información.",
 }: WhatsAppBotProps) {
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-
   const WhatsappLink = `http://wa.me/${phone}?text=${encodeURIComponent(
     message
   )}`;
-
-  // useEffect(() => {
-  //   if (buttonRef.current) {
-  //     anime({
-  //       targets: buttonRef.current,
-  //       translateY: [50, 0], // desde abajo hacia arriba
-  //       opacity: [0, 1],
-  //       easing: "easeOutElastic(1, .8)",
-  //       duration: 1000,
-  //     });
-  //   }
-  // }, []);
 
   return (
     <IconButton
@@ -48,6 +32,7 @@ export function WhatsAppBot({
         backgroundColor: "#25D366",
         color: "white",
         boxShadow: "0px 4px 12px rgba(37, 211, 102, 0.5)",
+        zIndex: 9999,
         "&:hover": {
           backgroundColor: "#1DA851",
           transform: "scale(1.1)",
