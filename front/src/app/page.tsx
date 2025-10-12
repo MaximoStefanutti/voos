8308c7ca6341b2195e6a45c98ff92e99da008363
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2025-10-12 10:44:44 -0300

    prueba sin animejs

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import { Dashboard } from "./pages/dashboard/page";
-import { AboutSection } from "./pages/about/page";
-import { ServicesSection } from "./pages/treatment/page";
+import Dashboard from "./pages/dashboard/page";
+import AboutSection from "./pages/about/page";
+import ServicesSection from "./pages/treatment/page";
 import { NavBar } from "./components/layout/navbar/NavBar";
 import { Footer } from "./components/layout/footer/footer";
 import SplashText from "./components/ui/text/SpalshText";
